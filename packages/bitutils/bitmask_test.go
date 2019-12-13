153adb319f95d70e47afcad9959cc2caeba815a0
IoTMOD <info@iotmod.de>
2019-12-13 21:32:13 +0100

    Update test for bitutils/bitmask

--- packages/bitutils/bitmask_test.go
+++ packages/bitutils/bitmask_test.go
@@ -14,20 +14,20 @@ func TestBitmask(t *testing.T) {
 		t.Error("flag at pos 1 should not be set")
 	}
 
-	b = b.SetFlag(0)
+	b = b.SettingFlag(0)
 	if !b.HasFlag(0) {
 		t.Error("flag at pos 0 should be set")
 	}
-	b = b.SetFlag(1)
+	b = b.SettingFlag(1)
 	if !b.HasFlag(1) {
 		t.Error("flag at pos 1 should be set")
 	}
 
-	b = b.ClearFlag(0)
+	b = b.ModifyingFlag(0, false)
 	if b.HasFlag(0) {
 		t.Error("flag at pos 0 should not be set")
 	}
-	b = b.ClearFlag(1)
+	b = b.ClearingFlag(1)
 	if b.HasFlag(1) {
 		t.Error("flag at pos 1 should not be set")
 	}
