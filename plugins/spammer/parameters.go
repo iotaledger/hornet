453accf9c52140a18eea39ba10e1080289b8bebb
muXxer <mux3r@web.de>
2019-12-15 15:03:06 +0100

    Changed default spammer settings

--- plugins/spammer/parameters.go
+++ plugins/spammer/parameters.go
@@ -3,9 +3,9 @@ package spammer
 import flag "github.com/spf13/pflag"
 
 func init() {
-	flag.String("spammer.address", "TANGLEKIT99SPAMMER99INTEGRATED99IN99HORNET999999999999999999999999999999999999999", "Tx Address")
+	flag.String("spammer.address", "HORNET99INTEGRATED99SPAMMER999999999999999999999999999999999999999999999999999999", "Tx Address")
 	flag.String("spammer.message", "Spamming with HORNET tipselect", "Message of the Tx")
-	flag.String("spammer.tag", "TANGLEKIT9SPAMMER99HORNET99", "Tag of the Tx")
+	flag.String("spammer.tag", "HORNET99INTEGRATED99SPAMMER", "Tag of the Tx")
 	flag.Uint("spammer.depth", 3, "Depth of the random walker")
 	flag.Float64("spammer.tpsRateLimit", 0.10, "Rate limit for the spam (0 = no limit)")
 	flag.Uint("spammer.workers", 1, "How many spammers should run in parallel")
