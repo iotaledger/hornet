41e2b22f4c96261f778f617ba0a5707f3c722bc9
IoTMOD <info@iotmod.de>
2020-01-08 13:02:11 +0100

    fix: Simplify download script

--- deb_files/download_snapshot.sh
+++ deb_files/download_snapshot.sh
@@ -3,9 +3,7 @@
 set -e
 set -u
 
-finalurl() { curl --silent --location --head --output /dev/null --write-out '%{url_effective}' -- "$@"; }
-
-REMOTE_FILE=$(finalurl https://dbfiles.iota.org/mainnet/hornet/latest-export.gz.bin)
+REMOTE_FILE="https://dbfiles.iota.org/mainnet/hornet/latest-export.gz.bin"
 
 # Download with timestamping (only download if remote file is newer)
-wget -N "${REMOTE_FILE}"
+wget -N -q ${REMOTE_FILE}
