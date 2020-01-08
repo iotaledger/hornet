9769acab4aa27ebc23a94a730888af46be94c013
IoTMOD <info@iotmod.de>
2020-01-08 17:33:42 +0100

    Add download note to snapshot download script

--- deb_files/download_snapshot.sh
+++ deb_files/download_snapshot.sh
@@ -6,4 +6,6 @@ set -u
 REMOTE_FILE="https://dbfiles.iota.org/mainnet/hornet/latest-export.gz.bin"
 
 # Download with timestamping (only download if remote file is newer)
+echo "Checking for the latest snapshot file..."
+echo "This may take a while"
 wget -N -q ${REMOTE_FILE}
