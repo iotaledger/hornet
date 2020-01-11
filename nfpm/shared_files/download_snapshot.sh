7d319b543e7eea4f9761bf99ec94f9b5ae1b1ee6
Nuriel Shem-Tov <nurielst@hotmail.com>
2020-01-12 00:07:43 +0100

    Add RPM package builds

--- nfpm/shared_files/download_snapshot.sh
+++ nfpm/shared_files/download_snapshot.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
 set -e
-set -u
 
 REMOTE_FILE="https://dbfiles.iota.org/mainnet/hornet/latest-export.gz.bin"
 
 # Download with timestamping (only download if remote file is newer)
 echo "Checking for the latest snapshot file..."
 echo "This may take a while"
-wget -N -q ${REMOTE_FILE}
+wget -N -q "${REMOTE_FILE}"
--- scripts/build_goreleaser_snapshot.sh
+++ scripts/build_goreleaser_snapshot.sh
@@ -4,17 +4,11 @@
 # Allows building of dirty git state and will not push results to github.
 
 # make script executable independent of path
-SCRIPTDIR=$(dirname "$0")
-pushd "${SCRIPTDIR}" >/dev/null
-pushd ".." >/dev/null
-
+trap 'cd $(dirs -l -0)' EXIT INT QUIT TERM
+cd ..
 
 GORELEASER_IMAGE=iotmod/goreleaser-cgo-cross-compiler:1.13.5
 REPO_PATH="/build"
 
 docker pull "${GORELEASER_IMAGE}"
 docker run --rm --privileged -v "${PWD}":"${REPO_PATH}" -w "${REPO_PATH}" "${GORELEASER_IMAGE}" goreleaser --rm-dist --snapshot --skip-publish
-
-
-popd >/dev/null
-popd >/dev/null
