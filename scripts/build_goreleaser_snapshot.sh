9efba532e6eed4613449ffe0a12b62ce4eeb9c8e
IoTMOD <info@iotmod.de>
2020-02-28 17:32:45 +0100

    feat: Update to Go 1.14

--- scripts/build_goreleaser_snapshot.sh
+++ scripts/build_goreleaser_snapshot.sh
@@ -6,7 +6,7 @@
 # make script executable independent of path
 cd $(dirname "$0")/../
 
-GORELEASER_IMAGE=iotmod/goreleaser-cgo-cross-compiler:1.13.8
+GORELEASER_IMAGE=iotmod/goreleaser-cgo-cross-compiler:1.14
 REPO_PATH="/build"
 
 docker pull "${GORELEASER_IMAGE}"
