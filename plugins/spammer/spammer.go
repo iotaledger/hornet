3ac5339ed2db558bd829415849861096eac073d1
Alexander Sporn <github@alexsporn.de>
2020-02-29 19:25:08 +0100

    Fixed passing int instead of MilestoneIndex while injecting tx from API or from spammer (#207)

--- plugins/spammer/spammer.go
+++ plugins/spammer/spammer.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/gohornet/hornet/packages/compressed"
 	"github.com/gohornet/hornet/packages/model/hornet"
+	"github.com/gohornet/hornet/packages/model/milestone_index"
 	"github.com/gohornet/hornet/plugins/gossip"
 	"github.com/gohornet/hornet/plugins/tipselection"
 )
@@ -127,7 +128,7 @@ func broadcastTransaction(tx *transaction.Transaction) error {
 	txBytesTruncated := compressed.TruncateTx(trinary.MustTritsToBytes(txTrits))
 	hornetTx := hornet.NewTransaction(tx, txBytesTruncated)
 
-	gossip.Events.ReceivedTransaction.Trigger(hornetTx, false, 0)
+	gossip.Events.ReceivedTransaction.Trigger(hornetTx, false, milestone_index.MilestoneIndex(0))
 	gossip.BroadcastTransaction(make(map[string]struct{}), txBytesTruncated, trinary.MustTrytesToBytes(hornetTx.GetHash())[:49])
 
 	return nil
