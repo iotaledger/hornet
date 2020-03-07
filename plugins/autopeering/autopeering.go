842c065f5cbe3177e15196a33fab19343064d38a
IoTMOD <info@iotmod.de>
2020-03-07 16:07:37 +0100

    code cleanup

--- plugins/autopeering/autopeering.go
+++ plugins/autopeering/autopeering.go
@@ -123,7 +123,7 @@ func start(shutdownSignal <-chan struct{}) {
 	log.Infof(name+" started: Address=%s/%s", peeringAddr.String(), peeringAddr.Network())
 
 	ID = lPeer.ID().String()
-	log.Infof(name+" started: ID=%s PublicKey=%s", lPeer.ID(), base64.StdEncoding.EncodeToString(lPeer.PublicKey()))
+	log.Infof(name+" started: ID=%s PublicKey=%s", ID, base64.StdEncoding.EncodeToString(lPeer.PublicKey()))
 
 	<-shutdownSignal
 	log.Info("Stopping Autopeering ...")
