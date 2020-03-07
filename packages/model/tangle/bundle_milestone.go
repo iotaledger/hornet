921056bc059739bdce72dd6afdf6421d58cfd393
muXxer <mux3r@web.de>
2020-03-07 16:46:25 +0100

    Fix bundle construction race condition (#220)
    * Fix bundle construction race condition
    
    * Praise the linter god
    
    * Trigger events outside of ComputeIfAbsent
    
    Co-authored-by: Alexander Sporn <github@alexsporn.de>

--- packages/model/tangle/bundle_milestone.go
+++ packages/model/tangle/bundle_milestone.go
@@ -7,6 +7,9 @@ import (
 )
 
 func (bundle *Bundle) setMilestone(milestone bool) {
+	bundle.Lock()
+	defer bundle.Unlock()
+
 	if milestone != bundle.metadata.HasFlag(HORNET_BUNDLE_METADATA_IS_MILESTONE) {
 		bundle.metadata = bundle.metadata.ModifyFlag(HORNET_BUNDLE_METADATA_IS_MILESTONE, milestone)
 		bundle.SetModified(true)
@@ -16,13 +19,11 @@ func (bundle *Bundle) setMilestone(milestone bool) {
 func (bundle *Bundle) IsMilestone() bool {
 	bundle.RLock()
 	defer bundle.RUnlock()
+
 	return bundle.metadata.HasFlag(HORNET_BUNDLE_METADATA_IS_MILESTONE)
 }
 
 func (bundle *Bundle) GetMilestoneIndex() milestone_index.MilestoneIndex {
-	bundle.RLock()
-	defer bundle.RUnlock()
-
 	cachedTailTx := bundle.getTail() // tx +1
 	index := milestone_index.MilestoneIndex(trinary.TrytesToInt(cachedTailTx.GetTransaction().Tx.ObsoleteTag))
 	cachedTailTx.Release() // tx -1
@@ -30,8 +31,5 @@ func (bundle *Bundle) GetMilestoneIndex() milestone_index.MilestoneIndex {
 }
 
 func (bundle *Bundle) GetMilestoneHash() trinary.Hash {
-	bundle.RLock()
-	defer bundle.RUnlock()
-
 	return bundle.tailTx
 }
