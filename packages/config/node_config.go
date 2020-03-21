13ab92f628277ee26d04af612776e1a9c76c2c34
IoTMOD <info@iotmod.de>
2020-03-21 14:06:26 +0100

    small fix

--- packages/config/node_config.go
+++ packages/config/node_config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	// CfgNodeAlias set an alias to identify a node
-	CfgNodeAlias = "node.Alias"
+	CfgNodeAlias = "node.alias"
 	// CfgNodeShowAliasInGetNodeInfo defines whether to show the alias in getNodeInfo
 	CfgNodeShowAliasInGetNodeInfo = "node.showAliasInGetNodeInfo"
 )
