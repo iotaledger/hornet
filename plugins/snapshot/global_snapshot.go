1bef6bbb35f31b1531a53713ff65bf56f05adde3
muXxer <mux3r@web.de>
2020-03-22 15:27:45 +0100

    Use bufio.Scanner for spentAddr

--- plugins/snapshot/global_snapshot.go
+++ plugins/snapshot/global_snapshot.go
@@ -2,7 +2,6 @@ package snapshot
 
 import (
 	"bufio"
-	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -31,26 +30,20 @@ func loadSpentAddresses(filePathSpent string) (int, error) {
 	}
 	defer spentFile.Close()
 
-	var line string
+	scanner := bufio.NewScanner(spentFile)
+	for scanner.Scan() {
+		addr := scanner.Text()
 
-	ioReader := bufio.NewReader(spentFile)
-	for {
-		line, err = ioReader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
+		if err := address.ValidAddress(addr); err != nil {
 			return 0, err
 		}
 
-		address := line[:len(line)-1]
-		if err := trinary.ValidTrytes(address); err != nil {
-			return 0, err
-		}
-
-		if tangle.MarkAddressAsSpent(address) {
+		if tangle.MarkAddressAsSpent(addr) {
 			spentAddressesCount++
 		}
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err
 	}
 
 	log.Infof("Finished loading spent addresses from %v", filePathSpent)
@@ -97,10 +90,9 @@ func loadSnapshotFromTextfiles(filePathLedger string, filePathsSpent []string, s
 		}
 
 		ledgerState[addr] = balance
-		//log.Infof("Address: %v (%d)", addr, balance)
 	}
-	if scanner.Err() != nil {
-		return errors.Wrapf(ErrSnapshotImportFailed, "Scanner: %v", scanner.Err())
+	if err := scanner.Err(); err != nil {
+		return errors.Wrapf(ErrSnapshotImportFailed, "Scanner: %v", err)
 	}
 
 	var total uint64
