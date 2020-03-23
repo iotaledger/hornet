ad99aeefb20abe4b355eb4a6bebe8793e6ac6ef2
muXxer <mux3r@web.de>
2020-03-23 22:11:48 +0100

    Add debug api call searchConfirmedApprover

--- plugins/webapi/debug.go
+++ plugins/webapi/debug.go
@@ -1,18 +1,155 @@
 package webapi
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gohornet/hornet/packages/model/tangle"
 	"github.com/gohornet/hornet/plugins/gossip"
+	"github.com/iotaledger/hive.go/daemon"
+	"github.com/iotaledger/iota.go/guards"
+	"github.com/iotaledger/iota.go/trinary"
+	"github.com/mitchellh/mapstructure"
 )
 
 func init() {
 	addEndpoint("getRequests", getRequests, implementedAPIcalls)
+	addEndpoint("searchConfirmedApprover", searchConfirmedApprover, implementedAPIcalls)
 }
 
 func getRequests(i interface{}, c *gin.Context, abortSignal <-chan struct{}) {
 	grr := &GetRequestsReturn{}
 	grr.Requests = gossip.RequestQueue.DebugRequests()
 	c.JSON(http.StatusOK, grr)
+}
+
+func createConfirmedApproverResult(confirmedTxHash trinary.Hash, path []bool) ([]*ApproverStruct, error) {
+
+	tanglePath := make([]*ApproverStruct, 0)
+
+	txHash := confirmedTxHash
+	for len(path) > 0 {
+		cachedTx := tangle.GetCachedTransactionOrNil(txHash) // tx +1
+		if cachedTx == nil {
+			return nil, fmt.Errorf("createConfirmedApproverResult: Transaction not found: %v", txHash)
+		}
+
+		isTrunk := path[len(path)-1]
+		path = path[:len(path)-1]
+
+		var nextTxHash trinary.Hash
+		if isTrunk {
+			nextTxHash = cachedTx.GetTransaction().GetTrunk()
+		} else {
+			nextTxHash = cachedTx.GetTransaction().GetBranch()
+		}
+		cachedTx.Release(true)
+
+		tanglePath = append(tanglePath, &ApproverStruct{TxHash: nextTxHash, ReferencedByTrunk: isTrunk})
+		txHash = nextTxHash
+	}
+
+	return tanglePath, nil
+}
+
+func searchConfirmedApprover(i interface{}, c *gin.Context, abortSignal <-chan struct{}) {
+	e := ErrorReturn{}
+	query := &SearchConfirmedApprover{}
+	result := &SearchConfirmedApproverReturn{}
+
+	err := mapstructure.Decode(i, query)
+	if err != nil {
+		e.Error = "Internal error"
+		c.JSON(http.StatusInternalServerError, e)
+		return
+	}
+
+	if !guards.IsTransactionHash(query.TxHash) {
+		e.Error = fmt.Sprintf("Invalid hash supplied: %s", query.TxHash)
+		c.JSON(http.StatusBadRequest, e)
+		return
+	}
+
+	txsToTraverse := make(map[trinary.Hash][]bool)
+	txsToTraverse[query.TxHash] = make([]bool, 0)
+
+	// Collect all tx to check by traversing the tangle
+	// Loop as long as new transactions are added in every loop cycle
+	for len(txsToTraverse) != 0 {
+		for txHash := range txsToTraverse {
+
+			if daemon.IsStopped() {
+				e.Error = "operation aborted"
+				c.JSON(http.StatusInternalServerError, e)
+				return
+			}
+
+			cachedTx := tangle.GetCachedTransactionOrNil(txHash) // tx +1
+			if cachedTx == nil {
+				delete(txsToTraverse, txHash)
+				log.Warnf("searchConfirmedApprover: Transaction not found: %v", txHash)
+				continue
+			}
+
+			confirmed, at := cachedTx.GetMetadata().GetConfirmed()
+			isTailTx := cachedTx.GetTransaction().IsTail()
+
+			cachedTx.Release(true) // tx -1
+
+			if confirmed {
+				resultFound := false
+
+				if query.SearchMilestone {
+					if isTailTx {
+						// Check if the bundle is a milestone, otherwise go on
+						cachedBndl := tangle.GetCachedBundleOrNil(txHash)
+						if cachedBndl != nil {
+							if cachedBndl.GetBundle().IsMilestone() {
+								resultFound = true
+							}
+							cachedBndl.Release(true)
+						}
+					}
+				} else {
+					resultFound = true
+				}
+
+				if resultFound {
+					approversResult, err := createConfirmedApproverResult(txHash, txsToTraverse[txHash])
+					if err != nil {
+						e.Error = err.Error()
+						c.JSON(http.StatusInternalServerError, e)
+						return
+					}
+
+					result.ConfirmedTxHash = txHash
+					result.ConfirmedByMilestoneIndex = uint32(at)
+					result.TanglePath = approversResult
+
+					c.JSON(http.StatusOK, result)
+					return
+				}
+			}
+
+			approverHashes := tangle.GetApproverHashes(txHash, true)
+			for _, approverHash := range approverHashes {
+
+				approverTx := tangle.GetCachedTransactionOrNil(approverHash) // tx +1
+				if approverTx == nil {
+					log.Warnf("searchConfirmedApprover: Approver not found: %v", approverHash)
+					continue
+				}
+
+				txsToTraverse[approverHash] = append(txsToTraverse[txHash], approverTx.GetTransaction().GetTrunk() == txHash)
+				approverTx.Release(true) // tx -1
+			}
+
+			delete(txsToTraverse, txHash)
+		}
+	}
+
+	e.Error = fmt.Sprintf("No confirmed approver found: %s", query.TxHash)
+	c.JSON(http.StatusInternalServerError, e)
+	return
 }
