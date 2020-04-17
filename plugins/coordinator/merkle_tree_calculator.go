6d7b6e528bb2f9fabaf526931aed3f1ab8a044b6
muXxer <mux3r@web.de>
2020-04-17 21:57:41 +0200

    Rename files

--- plugins/coordinator/merkle_tree_calculator.go
+++ plugins/coordinator/merkle_tree_calculator.go
@@ -14,6 +14,7 @@ import (
 	"github.com/iotaledger/iota.go/trinary"
 )
 
+// CreateMerkleTreeFile calculates the merkle tree and stores it into a file
 func CreateMerkleTreeFile(filePath string, seed trinary.Hash, securityLvl int, depth int) error {
 
 	ts := time.Now()
@@ -31,7 +32,7 @@ func CreateMerkleTreeFile(filePath string, seed trinary.Hash, securityLvl int, d
 
 	mt.Root = mt.Layers[0].Hashes[0]
 
-	if err := WriteMerkleTreeFile(filePath, mt); err != nil {
+	if err := StoreMerkleTreeFile(filePath, mt); err != nil {
 		return err
 	}
 
