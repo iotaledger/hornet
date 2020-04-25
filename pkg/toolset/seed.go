b7fa381647f709c8390dbccccfa091e0a6889733
muXxer <mux3r@web.de>
2020-04-25 12:40:48 +0200

    Use crypto/rand in seedGen tool

--- pkg/toolset/seed.go
+++ pkg/toolset/seed.go
@@ -1,11 +1,11 @@
 package toolset
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"errors"
 	"fmt"
-	"math/rand"
-	"time"
+	"math/big"
 )
 
 func seedGen(args []string) error {
@@ -14,13 +14,16 @@ func seedGen(args []string) error {
 		return errors.New("too many arguments for 'seedGen'")
 	}
 
-	rand.Seed(time.Now().UnixNano())
-
 	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	lenLetterRunes := int64(len(letterRunes))
 
 	b := make([]rune, 32)
 	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+		nBig, err := rand.Int(rand.Reader, big.NewInt(lenLetterRunes))
+		if err != nil {
+			panic(err)
+		}
+		b[i] = letterRunes[nBig.Int64()]
 	}
 
 	fmt.Println("Your autopeering seed: ", base64.StdEncoding.EncodeToString([]byte(string(b))))
