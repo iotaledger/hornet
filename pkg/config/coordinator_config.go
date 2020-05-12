33a0bcbc0c867388b9da08ea61ed804f9013221b
Luca Moser <moser.luca@gmail.com>
2020-05-12 21:49:47 +0200

    remove duped config option

--- pkg/config/coordinator_config.go
+++ pkg/config/coordinator_config.go
@@ -37,5 +37,5 @@ func init() {
 	flag.String(CfgCoordinatorMerkleTreeFilePath, "coordinator.tree", "the path to the Merkle tree of the coordinator")
 	flag.Int(CfgCoordinatorIntervalSeconds, 60, "the interval milestones are issued")
 	flag.Int(CfgCoordinatorCheckpointTransactions, 5, "the amount of checkpoints issued between two milestones")
-	flag.Boolt(CfgCoordinatorWhiteFlag, true, "instruct the coordinator to include the merkle tree root hash of the tail transactions which were confirmed via the white-flag approach into the milestone bundle")
+	flag.Bool(CfgCoordinatorWhiteFlag, true, "instruct the coordinator to include the merkle tree root hash of the tail transactions which were confirmed via the white-flag approach into the milestone bundle")
 }
