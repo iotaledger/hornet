add5f4a048e2d41afeb2162a1f67d30a04ecd360
Alexander Sporn <github@alexsporn.de>
2020-05-29 02:25:13 +0200

    - Changed the avg tps/ctps/confirmation calculation and layout

--- plugins/dashboard/frontend/src/app/components/ConfirmedMilestoneChart.tsx
+++ plugins/dashboard/frontend/src/app/components/ConfirmedMilestoneChart.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import {Col, Row} from "react-bootstrap";
 import NodeStore from "app/stores/NodeStore";
 import {inject, observer} from "mobx-react";
 import {Bar} from "react-chartjs-2";
@@ -127,26 +126,39 @@ const timeChartOptions = Object.assign({
 @observer
 export default class ConfirmedMilestoneChart extends React.Component<Props, any> {
     render() {
+        let avgTPS = "";
+        let avgCTPS = "";
+        let avgRate = "";
+
+        if (this.props.nodeStore.collected_confirmed_ms_metrics.length > 0) {
+            avgTPS = (this.props.nodeStore.collected_confirmed_ms_metrics.map((v) => v.tps).reduce((a, b) => a + b) / this.props.nodeStore.collected_confirmed_ms_metrics.length).toFixed(2);
+            avgCTPS = (this.props.nodeStore.collected_confirmed_ms_metrics.map((v) => v.ctps).reduce((a, b) => a + b) / this.props.nodeStore.collected_confirmed_ms_metrics.length).toFixed(2);
+            avgRate = (this.props.nodeStore.collected_confirmed_ms_metrics.map((v) => v.conf_rate).reduce((a, b) => a + b) / this.props.nodeStore.collected_confirmed_ms_metrics.length).toFixed(2);
+        }
+
         return (
             <Card>
                 <Card.Body>
                     <Card.Title>Confirmed Milestones</Card.Title>
                     <If condition={!!this.props.nodeStore.last_confirmed_ms_metric.ctps}>
-                        <Row>
-                            <Col>
+                        <Col>
+                            <Row>
                                 <small>
-                                    CTPS: {(this.props.nodeStore.last_confirmed_ms_metric.ctps).toFixed(2)}.
+                                    TPS: {(this.props.nodeStore.last_confirmed_ms_metric.tps).toFixed(2)} (Avg. {avgTPS})
+                                </small>
+                            </Row>
+                            <Row>
+                                <small>
+                                    CTPS: {(this.props.nodeStore.last_confirmed_ms_metric.ctps).toFixed(2)} (Avg. {avgCTPS})
+                                </small>
+                            </Row>
+                            <Row>
+                                <small>
                                     Confirmation: {(this.props.nodeStore.last_confirmed_ms_metric.conf_rate).toFixed(2)}%
+                                    (Avg. {avgRate}%)
                                 </small>
-                            </Col>
-                            <Col>
-                                <small>
-                                    Avg. TPS: {(this.props.nodeStore.confirmedMilestonesSeries.datasets[0].data.reduce((a, b) => a + b) / this.props.nodeStore.confirmedMilestonesSeries.datasets[0].data.length).toFixed(2)}.
-                                    Avg. CTPS: {(this.props.nodeStore.confirmedMilestonesSeries.datasets[1].data.reduce((a, b) => a + b) / this.props.nodeStore.confirmedMilestonesSeries.datasets[1].data.length).toFixed(2)}.
-                                    Avg. Confirmation: {((this.props.nodeStore.confirmedMilestonesSeries.datasets[1].data.reduce((a, b) => a + b) / this.props.nodeStore.confirmedMilestonesSeries.datasets[1].data.length)/(this.props.nodeStore.confirmedMilestonesSeries.datasets[0].data.reduce((a, b) => a + b) / this.props.nodeStore.confirmedMilestonesSeries.datasets[0].data.length)*100).toFixed(2)}%
-                                </small>
-                            </Col>
-                        </Row>
+                            </Row>
+                        </Col>
                     </If>
                     <div className={style.hornetChartSmall}>
                         <Bar data={this.props.nodeStore.confirmedMilestonesSeries}
