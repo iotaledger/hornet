75637e2ee14694da1062143c078257ddc3242096
muXxer <mux3r@web.de>
2020-06-03 22:25:38 +0200

    Do not accept autopeering peers if already statically connected

--- plugins/autopeering/plugin.go
+++ plugins/autopeering/plugin.go
@@ -103,6 +103,15 @@ func configureEvents() {
 
 		// whitelist the peer
 		originAddr, _ := iputils.ParseOriginAddress(gossipAddr)
+
+		// check if the peer is already statically peered
+		if peering.Manager().IsStaticallyPeered([]string{originAddr.Addr}, originAddr.Port) {
+			log.Infof("peer is already statically peered. removing autopeered peer %s", originAddr.String())
+			if err := peering.Manager().Remove(gossipAddr); err != nil {
+				log.Errorf("couldn't remove autopeered peer %s: %s", originAddr.String(), err)
+			}
+			return
+		}
 		peering.Manager().Whitelist([]string{originAddr.Addr}, originAddr.Port, ev.Peer)
 	}))
 
