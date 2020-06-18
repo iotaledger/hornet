cf680d1c0b4e18a87f9472f35e10d34992b1b6b5
IoTMOD <info@iotmod.de>
2020-06-18 23:12:55 +0200

    Release 0.4.1-rc2

--- plugins/cli/plugin.go
+++ plugins/cli/plugin.go
@@ -21,7 +21,7 @@ import (
 
 var (
 	// AppVersion version number
-	AppVersion          = "0.4.1-rc1"
+	AppVersion          = "0.4.1-rc2"
 	LatestGithubVersion = AppVersion
 
 	// AppName app code name
