5299bdabbdb4ed4ae062526949f87d89490be626
IoTMOD <info@iotmod.de>
2020-06-25 00:42:17 +0200

    Add/devnet config (#545)
    * add: Devnet config file
    
    * fix: AppVersion

--- plugins/cli/plugin.go
+++ plugins/cli/plugin.go
@@ -21,7 +21,7 @@ import (
 
 var (
 	// AppVersion version number
-	AppVersion          = "0.4.1-rc3"
+	AppVersion          = "0.4.1-rc4"
 	LatestGithubVersion = AppVersion
 
 	// AppName app code name
