fd50edcd71ff6f00cc8011bc17089456f1e6a005
muXxer <mux3r@web.de>
2020-07-19 00:06:40 +0200

    Revert "Skip check for confirmed tx in getTransactionRootSnapshotIndexes"
    This reverts commit c645ede445f318df7d3e52d9bc31859e0960e913.

--- pkg/tipselect/urts.go
+++ pkg/tipselect/urts.go
@@ -343,14 +343,19 @@ func (ts *TipSelector) getTransactionRootSnapshotIndexes(cachedTx *tangle.Cached
 		func(cachedTx *tangle.CachedTransaction) (bool, error) { // tx +1
 			defer cachedTx.Release(true) // tx -1
 
+			// first check if the tx was confirmed => update yrtsi and ortsi with the confirmation index
+			if confirmed, at := cachedTx.GetMetadata().GetConfirmed(); confirmed {
+				updateIndexes(at, at)
+				return false, nil
+			}
+
 			if bytes.Equal(startTxHash, cachedTx.GetTransaction().GetTxHash()) {
 				// skip the start transaction, so it doesn't get added to the outdatedTransactions
 				return true, nil
 			}
 
-			// if the tx contains recent (calculation index matches LSMI) information
-			// about yrtsi and ortsi (uncofirmed or confirmed), propagate that info
-			// no need to check confirmation index, since all confirmed txs get updated with yrtsi and ortsi equal confirmation index at confirmation
+			// if the tx was not confirmed yet, but already contains recent (calculation index matches LSMI) information
+			// about yrtsi and ortsi, propagate that info
 			yrtsi, ortsi, rtsci := cachedTx.GetMetadata().GetRootSnapshotIndexes()
 			if rtsci == lsmi {
 				updateIndexes(yrtsi, ortsi)
