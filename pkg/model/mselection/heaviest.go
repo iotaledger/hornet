273b8f02caa962aa30ac7027466d3acf1335a9df
muXxer <mux3r@web.de>
2020-07-24 10:31:28 +0200

    Add itemList struct

--- pkg/model/mselection/heaviest.go
+++ pkg/model/mselection/heaviest.go
@@ -51,6 +51,45 @@ type selectedTip struct {
 	index int
 }
 
+type itemList struct {
+	items []*item
+}
+
+// Len returns the length of the inner items slice.
+func (il *itemList) Len() int {
+	return len(il.items)
+}
+
+// randomTip selects a random tip item from the itemList.
+func (il *itemList) randomTip() (*selectedTip, error) {
+	if len(il.items) == 0 {
+		return nil, ErrNoTipsAvailable
+	}
+
+	index := utils.RandomInsecure(0, len(il.items)-1)
+	return &selectedTip{item: il.items[index], index: index}, nil
+}
+
+// applyTip set all bits of all referenced transactions of the tip in all existing tips to zero.
+// this way we can track which parts of the cone would already be referenced by this tip, and
+// correctly calculate the weight of the remaining tips.
+func (il *itemList) applyTip(tip *selectedTip) {
+
+	il.removeTip(tip)
+
+	// set all bits of all referenced transactions in all existing tips to zero
+	for _, otherTip := range il.items {
+		otherTip.refs.InPlaceDifference(tip.item.refs)
+	}
+}
+
+// removeTip removes the tip from the list if items.
+func (il *itemList) removeTip(tip *selectedTip) {
+	il.items[tip.index] = il.items[len(il.items)-1]
+	il.items[len(il.items)-1] = nil
+	il.items = il.items[:len(il.items)-1]
+}
+
 // New creates a new HeaviestSelector instance.
 func New(minHeaviestBranchUnconfirmedTransactionsThreshold int, maxHeaviestBranchTipsPerCheckpoint int, randomTipsPerCheckpoint int) *HeaviestSelector {
 	s := &HeaviestSelector{
@@ -77,13 +116,13 @@ func (s *HeaviestSelector) reset() {
 // selectTip selects a tip to be used for the next checkpoint.
 // it returns a tip, confirming the most transactions in the future cone.
 // the selection can be cancelled anytime via the provided context. in this case, it returns the current best solution.
-func (s *HeaviestSelector) selectTip(ctx context.Context, tips []*item) (*selectedTip, uint, error) {
+func (s *HeaviestSelector) selectTip(ctx context.Context, tipsList *itemList) (*selectedTip, uint, error) {
 
-	if len(tips) == 0 {
+	if tipsList.Len() == 0 {
 		return nil, 0, ErrNoTipsAvailable
 	}
 
-	lastTip := tips[len(tips)-1]
+	lastTip := tipsList.items[tipsList.Len()-1]
 
 	var best = struct {
 		tips  []*selectedTip
@@ -92,13 +131,13 @@ func (s *HeaviestSelector) selectTip(ctx context.Context, tips []*item) (*select
 		tips: []*selectedTip{
 			{
 				item:  lastTip,
-				index: len(tips) - 1,
+				index: tipsList.Len() - 1,
 			}},
 		count: lastTip.refs.Count(),
 	}
 
 	// loop through all tips and find the one with the most referenced transactions
-	for index, tip := range tips {
+	for index, tip := range tipsList.items {
 		// when the context has been cancelled, return the current best with an error
 		select {
 		case <-ctx.Done():
@@ -150,10 +189,10 @@ func (s *HeaviestSelector) SelectTips(enforceTips bool) (hornet.Hashes, error) {
 	// copy the tips to release the lock to allow faster iteration
 	// and to get a frozen view of the tangle, so an attacker can't
 	// create heavier branches while we are searching the best tips
-	tips := s.tipItems()
+	tipsList := s.copyTipItemsToList()
 
 	// tips could be empty after a reset
-	if len(tips) == 0 {
+	if tipsList.Len() == 0 {
 		return nil, ErrNoTipsAvailable
 	}
 
@@ -163,12 +202,11 @@ func (s *HeaviestSelector) SelectTips(enforceTips bool) (hornet.Hashes, error) {
 		// run the tip selection for at most 0.1s to keep the view on the tangle recent; this should be plenty
 		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(100*time.Millisecond))
 
-		tip, count, err := s.selectTip(ctx, tips)
+		tip, count, err := s.selectTip(ctx, tipsList)
+		cancel()
 		if err != nil && err != context.DeadlineExceeded {
-			cancel()
 			break
 		}
-		cancel()
 
 		// if we want to enforce tips, at least collect one
 		if count < uint(s.minHeaviestBranchUnconfirmedTransactionsThreshold) && !(enforceTips && len(result) == 0) {
@@ -176,7 +214,7 @@ func (s *HeaviestSelector) SelectTips(enforceTips bool) (hornet.Hashes, error) {
 			break
 		}
 
-		tips = applyTip(tips, tip)
+		tipsList.applyTip(tip)
 		result = append(result, tip.item.hash)
 	}
 
@@ -186,12 +224,12 @@ func (s *HeaviestSelector) SelectTips(enforceTips bool) (hornet.Hashes, error) {
 
 	// also pick random tips if at least one heaviest branch tip was found
 	for i := 0; i < s.randomTipsPerCheckpoint; i++ {
-		item, err := randomTipFromItems(tips)
+		item, err := tipsList.randomTip()
 		if err != nil {
 			break
 		}
 
-		tips = applyTip(tips, item)
+		tipsList.applyTip(item)
 		result = append(result, item.item.hash)
 	}
 
@@ -230,34 +268,14 @@ func (s *HeaviestSelector) OnNewSolidBundle(bndl *tangle.Bundle) (tipCount int, 
 
 	// we have to check the below max depth criteria for approvees that do not reference our future cone.
 	// if all the unknown approvees do not fail the below max depth criteria, the tip is valid
-	if len(approveeHashes) > 0 {
-		lsmi := tangle.GetSolidMilestoneIndex()
-
-		for approveeHash := range approveeHashes {
-			var approveeORTSI milestone.Index
-
-			if tangle.SolidEntryPointsContain(hornet.Hash(approveeHash)) {
-				// if the approvee is an solid entry point, use the EntryPointIndex as ORTSI
-				approveeORTSI = tangle.GetSnapshotInfo().EntryPointIndex
-			} else {
-				cachedApproveeTx := tangle.GetCachedTransactionOrNil(hornet.Hash(approveeHash)) // tx +1
-				if cachedApproveeTx == nil {
-					panic(fmt.Sprintf("transaction not found: %v", hornet.Hash(approveeHash).Trytes()))
-				}
-
-				_, approveeORTSI = dag.GetTransactionRootSnapshotIndexes(cachedApproveeTx.Retain(), lsmi) // tx +1
-				cachedApproveeTx.Release(true)
-			}
-
-			// if the approveeORTSI to LSMI delta of the approvee is equal or greater belowMaxDepth, the tip is invalid.
-			// "equal" is important because the next milestone would reference this transaction.
-			if lsmi-approveeORTSI >= belowMaxDepth {
-				return s.GetStats()
-			}
-		}
+	if !checkBelowMaxDepth(approveeHashes) {
+		return s.GetStats()
 	}
 
 	// compute the referenced transactions
+	// all the known approvers in the HeaviestSelector are represented by a unique bit in a bitset.
+	// if a new approver is added, we expand the bitset by 1 bit and store the Union of the bitsets
+	// of trunk and branch for this approver, to know which parts of the cone are referenced by this approver.
 	idx := uint(len(s.approvers))
 	it := &item{hash: bndl.GetTailHash(), refs: bitset.New(idx + 1).Set(idx)}
 	if trunkItem != nil {
@@ -285,8 +303,8 @@ func (s *HeaviestSelector) removeTip(it *item) {
 	it.tip = nil
 }
 
-// tipItems returns a copy of the items corresponding to tips.
-func (s *HeaviestSelector) tipItems() []*item {
+// copyTipItemsToList returns a copy of the items corresponding to tips.
+func (s *HeaviestSelector) copyTipItemsToList() *itemList {
 	s.Lock()
 	defer s.Unlock()
 
@@ -294,12 +312,46 @@ func (s *HeaviestSelector) tipItems() []*item {
 	for e := s.tips.Front(); e != nil; e = e.Next() {
 		result = append(result, e.Value.(*item))
 	}
-	return result
+	return &itemList{items: result}
 }
 
 // GetStats returns the amount of known tips and approvees of s.
 func (s *HeaviestSelector) GetStats() (tipCount int, approveeCount int) {
 	return s.tips.Len(), len(s.approvers)
+}
+
+// checkBelowMaxDepth checks the below max depth criteria for the given approvees.
+// if one of the approvees fails the below max depth criteria, the result is false.
+func checkBelowMaxDepth(approveeHashes map[string]struct{}) bool {
+
+	if len(approveeHashes) > 0 {
+		lsmi := tangle.GetSolidMilestoneIndex()
+
+		for approveeHash := range approveeHashes {
+			var approveeORTSI milestone.Index
+
+			if tangle.SolidEntryPointsContain(hornet.Hash(approveeHash)) {
+				// if the approvee is an solid entry point, use the EntryPointIndex as ORTSI
+				approveeORTSI = tangle.GetSnapshotInfo().EntryPointIndex
+			} else {
+				cachedApproveeTx := tangle.GetCachedTransactionOrNil(hornet.Hash(approveeHash)) // tx +1
+				if cachedApproveeTx == nil {
+					panic(fmt.Sprintf("transaction not found: %v", hornet.Hash(approveeHash).Trytes()))
+				}
+
+				_, approveeORTSI = dag.GetTransactionRootSnapshotIndexes(cachedApproveeTx.Retain(), lsmi) // tx +1
+				cachedApproveeTx.Release(true)
+			}
+
+			// if the approveeORTSI to LSMI delta of the approvee is equal or greater belowMaxDepth, the tip is invalid.
+			// "equal" is important because the next milestone would reference this transaction.
+			if lsmi-approveeORTSI >= belowMaxDepth {
+				return false
+			}
+		}
+	}
+
+	return true
 }
 
 // randomTipFromTips selects a random tip from the provided slice of tips.
@@ -309,34 +361,4 @@ func randomTipFromTips(tips []*selectedTip) (*selectedTip, error) {
 	}
 
 	return tips[utils.RandomInsecure(0, len(tips)-1)], nil
-}
-
-// randomTipFromItems selects a random tip item from the provided slice of items.
-func randomTipFromItems(tips []*item) (*selectedTip, error) {
-	if len(tips) == 0 {
-		return nil, ErrNoTipsAvailable
-	}
-
-	index := utils.RandomInsecure(0, len(tips)-1)
-	return &selectedTip{item: tips[index], index: index}, nil
-}
-
-// applyTip set all bits of all referenced transactions of the tip in all existing tips to zero.
-func applyTip(tips []*item, tip *selectedTip) []*item {
-
-	tips = removeTip(tips, tip)
-
-	// set all bits of all referenced transactions in all existing tips to zero
-	for _, otherTip := range tips {
-		otherTip.refs.InPlaceDifference(tip.item.refs)
-	}
-
-	return tips
-}
-
-// removeTip removes the tip from the list if items.
-func removeTip(tips []*item, tip *selectedTip) []*item {
-	tips[tip.index] = tips[len(tips)-1]
-	tips[len(tips)-1] = nil
-	return tips[:len(tips)-1]
 }
