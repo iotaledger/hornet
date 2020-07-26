ae2b6ff009280be32991d85569bd145e52f891a1
muXxer <mux3r@web.de>
2020-07-26 21:39:18 +0200

    Release 0.5.0-rc3 (#576)

--- plugins/cli/plugin.go
+++ plugins/cli/plugin.go
@@ -21,7 +21,7 @@ import (
 
 var (
 	// AppVersion version number
-	AppVersion          = "0.5.0-rc2"
+	AppVersion          = "0.5.0-rc3"
 	LatestGithubVersion = AppVersion
 
 	// AppName app code name
