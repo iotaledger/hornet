f0953a68edd948bffaf500c9c601a40901a3fd47
Luca Moser <moser.luca@gmail.com>
2020-09-02 09:31:11 +0200

    removes debug print in reconnect.go

--- pkg/peering/reconnect.go
+++ pkg/peering/reconnect.go
@@ -95,7 +95,6 @@ next:
 // adds the given peers to the reconnect pool.
 func (m *Manager) moveInitialPeersToReconnectPool(peers []*config.PeerConfig) {
 	for _, peerConf := range peers {
-		fmt.Println("conf", peerConf)
 		if peerConf.ID == "" {
 			continue
 		}
