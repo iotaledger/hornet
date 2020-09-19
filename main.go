2b53ddfbb993d3401ad190db9f4c30bf37e72eab
muXxer <mux3r@web.de>
2020-09-19 18:00:44 +0200

    Temporarily remove ZMQ and MQTT for now

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/gohornet/hornet/plugins/gossip"
 	"github.com/gohornet/hornet/plugins/gracefulshutdown"
 	"github.com/gohornet/hornet/plugins/metrics"
-	"github.com/gohornet/hornet/plugins/mqtt"
 	"github.com/gohornet/hornet/plugins/peering"
 	"github.com/gohornet/hornet/plugins/pow"
 	"github.com/gohornet/hornet/plugins/profiling"
@@ -24,7 +23,6 @@ import (
 	"github.com/gohornet/hornet/plugins/urts"
 	"github.com/gohornet/hornet/plugins/warpsync"
 	"github.com/gohornet/hornet/plugins/webapi"
-	"github.com/gohornet/hornet/plugins/zmq"
 )
 
 func main() {
@@ -54,8 +52,6 @@ func main() {
 			metrics.PLUGIN,
 			snapshot.PLUGIN,
 			dashboard.PLUGIN,
-			zmq.PLUGIN,
-			mqtt.PLUGIN,
 			spammer.PLUGIN,
 			coordinator.PLUGIN,
 			prometheus.PLUGIN,
