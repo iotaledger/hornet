514778f1e78ff723df23105d93667f9b833e0b17
Luca Moser <moser.luca@gmail.com>
2020-10-13 09:31:12 +0200

    add .bat files

--- run_3rd.bat
+++ run_3rd.bat
@@ -1,6 +1,6 @@
-del /f /q /s alphanetdb2
-go run -tags "pow_avx" main.go -c config_alphanet2 ^
---profiling.bindAddress="127.0.0.1:6161" ^
---p2p.bindAddresses="/ip4/127.0.0.1/tcp/15601" ^
---p2p.peerStore.path="./p2pstore2" ^
---p2p.peers="/ip4/127.0.0.1/tcp/15600/p2p/12D3KooWFJ8Nq6gHLLvigTpPSbyMmLk35k1TcpJof8Y4y8yFAB32"
+del /f /q /s alphanetdb3
+go run -tags "pow_avx" main.go -c config_alphanet3 ^
+--profiling.bindAddress="127.0.0.1:6262" ^
+--p2p.bindAddresses="/ip4/127.0.0.1/tcp/15602" ^
+--p2p.peerStore.path="./p2pstore3" ^
+--p2p.peers="/ip4/127.0.0.1/tcp/15601/p2p/12D3KooWCM6qcDMYHmn827QB6swNw9mFxCBWtAex4KeXjbLzsRTG"
