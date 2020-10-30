558ebff8548b3429f3895bdba366fedea9dda132
Luca Moser <moser.luca@gmail.com>
2020-10-30 15:09:20 +0100

    heartbeats are encoded in little endian (#705)

--- pkg/protocol/gossip/sting.go
+++ pkg/protocol/gossip/sting.go
@@ -172,9 +172,9 @@ type Heartbeat struct {
 /// ParseHeartbeat parses the given message into a heartbeat.
 func ParseHeartbeat(data []byte) *Heartbeat {
 	return &Heartbeat{
-		SolidMilestoneIndex:  milestone.Index(binary.BigEndian.Uint32(data[:4])),
-		PrunedMilestoneIndex: milestone.Index(binary.BigEndian.Uint32(data[4:8])),
-		LatestMilestoneIndex: milestone.Index(binary.BigEndian.Uint32(data[8:12])),
+		SolidMilestoneIndex:  milestone.Index(binary.LittleEndian.Uint32(data[:4])),
+		PrunedMilestoneIndex: milestone.Index(binary.LittleEndian.Uint32(data[4:8])),
+		LatestMilestoneIndex: milestone.Index(binary.LittleEndian.Uint32(data[8:12])),
 		ConnectedNeighbors:   int(data[12]),
 		SyncedNeighbors:      int(data[13]),
 	}
