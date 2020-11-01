7e9590c731b9425327bad7f141428b7e6af2d076
muXxer <mux3r@web.de>
2020-11-01 14:36:15 +0100

    Clean up alphanet scripts

--- run_2nd.bat
+++ run_2nd.bat
@@ -1,6 +1,7 @@
 del /f /q /s alphanetdb2
-go run -tags "pow_avx" main.go -c config_alphanet2.json ^
---profiling.bindAddress="127.0.0.1:6161" ^
+go run -tags "pow_avx" main.go -c config_alphanet2.json \
+--profiling.bindAddress="127.0.0.1:6061" ^
 --p2p.bindMultiAddresses="/ip4/127.0.0.1/tcp/15601" ^
 --p2p.peerStore.path="./p2pstore2" ^
---p2p.peers="/ip4/127.0.0.1/tcp/15600/p2p/12D3KooWFJ8Nq6gHLLvigTpPSbyMmLk35k1TcpJof8Y4y8yFAB32,/ip4/127.0.0.1/tcp/15601/p2p/12D3KooWPEFNwbzoRWARYGYP4kR5LkR1PunAzkv4Ammo5CYq7SQq"
+--p2p.identityPrivateKey="a06b288ce7fc3b6f1e716f6f7d72050b53417aae4b305a68883550a3bb28597f254b082515a79391a7f13009b4133851a0c4d48e0e948809c3b46ff3e2500b4f" ^
+--p2p.peers="/ip4/127.0.0.1/tcp/15600/p2p/12D3KooWSagdVaCrS14GeJhM8CbQr41AW2PiYMgptTyAybCbQuEY,/ip4/127.0.0.1/tcp/15602/p2p/12D3KooWGdr8M5KX8KuKaXSiKfHJstdVnRkadYmupF7tFk2HrRoA"
--- run_3rd.bat
+++ run_3rd.bat
@@ -1,6 +1,7 @@
 del /f /q /s alphanetdb3
 go run -tags "pow_avx" main.go -c config_alphanet3.json ^
---profiling.bindAddress="127.0.0.1:6262" ^
+--profiling.bindAddress="127.0.0.1:6062" ^
 --p2p.bindMultiAddresses="/ip4/127.0.0.1/tcp/15602" ^
 --p2p.peerStore.path="./p2pstore3" ^
---p2p.peers="/ip4/127.0.0.1/tcp/15601/p2p/12D3KooWCM6qcDMYHmn827QB6swNw9mFxCBWtAex4KeXjbLzsRTG"
+--p2p.identityPrivateKey="5126767a84e1ced849dbbf2be809fd40f90bcfb81bd0d3309e2e25e34f803bf265500854f1f0e8fd3c389cf7b6b59cfd422b9319f257e2a8d3a772973560acdd" ^
+--p2p.peers="/ip4/127.0.0.1/tcp/15600/p2p/12D3KooWSagdVaCrS14GeJhM8CbQr41AW2PiYMgptTyAybCbQuEY,/ip4/127.0.0.1/tcp/15601/p2p/12D3KooWCKwcTWevoRKa2kEBputeGASvEBuDfRDSbe8t1DWugUmL"
