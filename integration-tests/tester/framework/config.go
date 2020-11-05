ca004b7c0a3b976c88a2fd348576f3b37c9d6896
Luca Moser <moser.luca@gmail.com>
2020-11-05 11:44:02 +0100

    Adds pow verification/handler (#717)
    * adds pow verification/handler
    
    * updates comment

--- integration-tests/tester/framework/config.go
+++ integration-tests/tester/framework/config.go
@@ -256,6 +256,8 @@ type CoordinatorConfig struct {
 	RunAsCoo bool
 	// Whether to run the coordinator in bootstrap node.
 	Bootstrap bool
+	// The minimum PoW score needed.
+	MinPoWScore float64
 	// The coo private keys.
 	PrivateKeys []string
 	// The coo public key ranges.
@@ -276,14 +278,16 @@ func (cooConfig *CoordinatorConfig) CLIFlags() []string {
 		fmt.Sprintf("--cooBootstrap=%v", cooConfig.Bootstrap),
 		fmt.Sprintf("--publicKeyRanges=[%v]", strings.Join(keyRanges, ",")),
 		fmt.Sprintf("--%s=%d", config.CfgCoordinatorIntervalSeconds, cooConfig.IssuanceIntervalSeconds),
+		fmt.Sprintf("--%s=%d", config.CfgCoordinatorMinPoWScore, cooConfig.MinPoWScore),
 	}
 }
 
 // DefaultCoordinatorConfig returns the default coordinator config.
 func DefaultCoordinatorConfig() CoordinatorConfig {
 	return CoordinatorConfig{
-		RunAsCoo:  false,
-		Bootstrap: false,
+		RunAsCoo:    false,
+		Bootstrap:   false,
+		MinPoWScore: 100,
 		PrivateKeys: []string{"651941eddb3e68cb1f6ef4ef5b04625dcf5c70de1fdc4b1c9eadb2c219c074e0ed3c3f1a319ff4e909cf2771d79fece0ac9bd9fd2ee49ea6c0885c9cb3b1248c",
 			"0e324c6ff069f31890d496e9004636fd73d8e8b5bea08ec58a4178ca85462325f6752f5f46a53364e2ee9c4d662d762a81efd51010282a75cd6bd03f28ef349c"},
 		PublicKeyRanges: []config.PublicKeyRange{
