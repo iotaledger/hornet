83e71f2ce70132fec8e0fff11883bf21725be3f6
Dr-Electron <61637085+Dr-Electron@users.noreply.github.com>
2021-01-09 23:08:52 +0100

    Remove git tag from build script (#818)

--- scripts/build_hornet.sh
+++ scripts/build_hornet.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
-# Builds HORNET with the latest git tag and commit hash (short)
-# E.g.: ./hornet -v --> HORNET 0.3.0-75316fe
+# Builds HORNET with the latest commit hash (short)
+# E.g.: ./hornet -v --> HORNET 75316fe
 
-latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 commit_hash=$(git rev-parse --short HEAD)
 
-go build -ldflags="-s -w -X github.com/gohornet/hornet/core/app.Version=${latest_tag:1}-$commit_hash"
+go build -ldflags="-s -w -X github.com/gohornet/hornet/core/app.Version=$commit_hash"
