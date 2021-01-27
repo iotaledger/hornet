442f0f448f8a2107be7e61e5477e1e5a08b93277
Alexander Sporn <github@alexsporn.de>
2021-01-27 02:21:33 +0100

    - Added new `type` filter to the outputs API calls

--- plugins/restapi/v1/utxo.go
+++ plugins/restapi/v1/utxo.go
@@ -145,10 +145,15 @@ func balanceByEd25519Address(c echo.Context) (*addressBalanceResponse, error) {
 	return ed25519Balance(&address)
 }
 
-func ed25519Outputs(address *iotago.Ed25519Address, includeSpent bool) (*addressOutputsResponse, error) {
+func ed25519Outputs(address *iotago.Ed25519Address, includeSpent bool, filterType *iotago.OutputType) (*addressOutputsResponse, error) {
 	maxResults := deps.NodeConfig.Int(restapiplugin.CfgRestAPILimitsMaxResults)
 
-	unspentOutputs, err := deps.UTXO.UnspentOutputsForAddress(address, false, maxResults)
+	var opts []utxo.UTXOIterateOption
+	if filterType != nil {
+		opts = append(opts, utxo.FilterOutputType(*filterType))
+	}
+
+	unspentOutputs, err := deps.UTXO.UnspentOutputsForAddress(address, append(opts, utxo.MaxResultCount(maxResults))...)
 	if err != nil {
 		return nil, errors.WithMessagef(restapi.ErrInternalError, "reading unspent outputs failed: %s, error: %s", address, err)
 	}
@@ -160,7 +165,7 @@ func ed25519Outputs(address *iotago.Ed25519Address, includeSpent bool) (*address
 
 	if includeSpent && maxResults-len(outputIDs) > 0 {
 
-		spents, err := deps.UTXO.SpentOutputsForAddress(address, false, maxResults-len(outputIDs))
+		spents, err := deps.UTXO.SpentOutputsForAddress(address, append(opts, utxo.MaxResultCount(maxResults-len(outputIDs)))...)
 		if err != nil {
 			return nil, errors.WithMessagef(restapi.ErrInternalError, "reading spent outputs failed: %s, error: %s", address, err)
 		}
@@ -185,9 +190,24 @@ func outputsIDsByBech32Address(c echo.Context) (*addressOutputsResponse, error) 
 		return nil, errors.WithMessage(restapi.ErrServiceUnavailable, "node is not synced")
 	}
 
-	addressParam := strings.ToLower(c.Param(ParameterAddress))
 	includeSpent, _ := strconv.ParseBool(strings.ToLower(c.QueryParam("include-spent")))
 
+	typeParam := strings.ToLower(c.QueryParam("type"))
+	var filteredType *iotago.OutputType
+
+	if len(typeParam) > 0 {
+		outputTypeInt, err := strconv.ParseInt(typeParam, 10, 32)
+		if err != nil {
+			return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid type: %s, error: unknown output type", typeParam)
+		}
+		outputType := iotago.OutputType(outputTypeInt)
+		if outputType != iotago.OutputSigLockedSingleOutput || outputType != iotago.OutputSigLockedDustAllowanceOutput {
+			return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid type: %s, error: unknown output type", typeParam)
+		}
+		filteredType = &outputType
+	}
+
+	addressParam := strings.ToLower(c.Param(ParameterAddress))
 	_, bech32Address, err := iotago.ParseBech32(addressParam)
 	if err != nil {
 		return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid address: %s, error: %s", addressParam, err)
@@ -198,7 +218,7 @@ func outputsIDsByBech32Address(c echo.Context) (*addressOutputsResponse, error) 
 		// TODO: implement
 		return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid address: %s, error: %s", addressParam, iotago.ErrWOTSNotImplemented)
 	case *iotago.Ed25519Address:
-		return ed25519Outputs(address, includeSpent)
+		return ed25519Outputs(address, includeSpent, filteredType)
 	default:
 		return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid address: %s, error: unknown address type", addressParam)
 	}
@@ -210,9 +230,23 @@ func outputsIDsByEd25519Address(c echo.Context) (*addressOutputsResponse, error)
 		return nil, errors.WithMessage(restapi.ErrServiceUnavailable, "node is not synced")
 	}
 
-	addressParam := strings.ToLower(c.Param(ParameterAddress))
 	includeSpent, _ := strconv.ParseBool(strings.ToLower(c.QueryParam("include-spent")))
 
+	var filteredType *iotago.OutputType
+	typeParam := strings.ToLower(c.QueryParam("type"))
+	if len(typeParam) > 0 {
+		outputTypeInt, err := strconv.ParseInt(typeParam, 10, 32)
+		if err != nil {
+			return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid type: %s, error: unknown output type", typeParam)
+		}
+		outputType := iotago.OutputType(outputTypeInt)
+		if outputType != iotago.OutputSigLockedSingleOutput || outputType != iotago.OutputSigLockedDustAllowanceOutput {
+			return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid type: %s, error: unknown output type", typeParam)
+		}
+		filteredType = &outputType
+	}
+
+	addressParam := strings.ToLower(c.Param(ParameterAddress))
 	addressBytes, err := hex.DecodeString(addressParam)
 	if err != nil {
 		return nil, errors.WithMessagef(restapi.ErrInvalidParameter, "invalid address: %s, error: %s", addressParam, err)
@@ -225,5 +259,5 @@ func outputsIDsByEd25519Address(c echo.Context) (*addressOutputsResponse, error)
 	var address iotago.Ed25519Address
 	copy(address[:], addressBytes)
 
-	return ed25519Outputs(&address, includeSpent)
+	return ed25519Outputs(&address, includeSpent, filteredType)
 }
