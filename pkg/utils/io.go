2329daefc18257c519d1c0ff72f52d374a39f8eb
Wolfgang Welz <welzwo@gmail.com>
2021-02-02 09:57:35 +0100

    add comments to utils/io

--- pkg/utils/io.go
+++ pkg/utils/io.go
@@ -5,6 +5,10 @@ import (
 	"os"
 )
 
+// ReadFromFile reads structured binary data from the file named by filename to data.
+// A successful call returns err == nil, not err == EOF.
+// ReadFromFile uses binary.Read to decode data. Data must be a pointer to a fixed-size value or a slice
+// of fixed-size values.
 func ReadFromFile(filename string, data interface{}) error {
 	f, err := os.Open(filename)
 	if err != nil {
@@ -14,6 +18,11 @@ func ReadFromFile(filename string, data interface{}) error {
 	return binary.Read(f, binary.LittleEndian, data)
 }
 
+// WriteToFile writes the binary representation of data to a file named by filename.
+// If the file does not exist, WriteFile creates it with permissions perm
+// (before umask); otherwise WriteFile truncates it before writing, without changing permissions.
+// WriteToFile uses binary.Write to encode data. Data must be a pointer to a fixed-size value or a slice
+// of fixed-size values.
 func WriteToFile(filename string, data interface{}, perm os.FileMode) (err error) {
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
 	if err != nil {
