972b3868ed8f29c3e61483ad1c5d55ebc916da22
Alexander Sporn <github@alexsporn.de>
2022-01-25 09:13:22 +0100

    Update integration-tests/tester/framework/config.go
    Co-authored-by: muXxer <git@muxxer.de>

--- integration-tests/tester/framework/config.go
+++ integration-tests/tester/framework/config.go
@@ -33,7 +33,7 @@ const (
 
 	GenesisAddressPublicKeyHex = "f7868ab6bb55800b77b8b74191ad8285a9bf428ace579d541fda47661803ff44"
 	GenesisAddressHex          = "6920b176f613ec7be59e68fc68f597eb3393af80f74c7c3db78198147d5f1f92"
-	GensisAddressBech32        = "atoi1qp5jpvtk7cf7c7l9ne50c684jl4n8ya0srm5clpak7qes9ratu0eysflmsz"
+	GenesisAddressBech32       = "atoi1qp5jpvtk7cf7c7l9ne50c684jl4n8ya0srm5clpak7qes9ratu0eysflmsz"
 
 	autopeeringMaxTries = 50
 
