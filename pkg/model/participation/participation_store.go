aab55f7bc6cb3378ad4ab3b570aed43b7005c007
Alexander Sporn <github@alexsporn.de>
2022-03-29 13:32:45 +0200

    - go fmt previous commit

--- pkg/model/participation/participation_store.go
+++ pkg/model/participation/participation_store.go
@@ -156,7 +156,7 @@ func participationKeyForEventPrefix(eventID EventID) []byte {
 func participationKeyForEventAndAddressPrefix(eventID EventID, addressBytes []byte) []byte {
 	m := marshalutil.New(66)
 	m.WriteBytes(participationKeyForEventPrefix(eventID)) // 33 bytes
-	m.WriteBytes(addressBytes)                                     // 33 bytes
+	m.WriteBytes(addressBytes)                            // 33 bytes
 	return m.Bytes()
 }
 
