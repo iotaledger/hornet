cf51ab9de4f1c7a9a71e4996484de83d11b7adef
Alexander Sporn <github@alexsporn.de>
2022-03-31 17:20:16 +0200

    - fix dir permissions when running private tangle

--- private_tangle/cleanup.sh
+++ private_tangle/cleanup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+mkdir -p privatedb
+mkdir -p snapshots
+chown -R 65532:65532 /privatedb
+chown -R 65532:65532 /snapshots
+
 docker-compose run cleanup
 docker-compose down --remove-orphans
