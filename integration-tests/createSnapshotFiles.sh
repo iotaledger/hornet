fe26b25ef395b0482a44ea99124f43b05f4ff675
Alexander Sporn <github@alexsporn.de>
2022-04-26 17:32:27 +0200

    - Changed usages of the `--networkID` flag to `--networkName`

--- integration-tests/createSnapshotFiles.sh
+++ integration-tests/createSnapshotFiles.sh
@@ -9,11 +9,11 @@ cd tools/intsnap && go run main.go && mv *.bin ../../integration-tests/assets/ &
 
 # migration tests
 echo "create migration test snapshot files..."
-rm integration-tests/assets/migration_full_snapshot.bin && go run main.go tools snap-gen --networkID alphanet1 --mintAddress 6920b176f613ec7be59e68fc68f597eb3393af80f74c7c3db78198147d5f1f92 --treasuryAllocation 10000000000 --outputPath integration-tests/assets/migration_full_snapshot.bin
+rm integration-tests/assets/migration_full_snapshot.bin && go run main.go tools snap-gen --networkName alphanet1 --mintAddress 6920b176f613ec7be59e68fc68f597eb3393af80f74c7c3db78198147d5f1f92 --treasuryAllocation 10000000000 --outputPath integration-tests/assets/migration_full_snapshot.bin
 
 # other tests
 echo "create other snapshot files..."
-rm integration-tests/assets/full_snapshot.bin && go run main.go tools snap-gen --networkID alphanet1 --mintAddress 6920b176f613ec7be59e68fc68f597eb3393af80f74c7c3db78198147d5f1f92 --treasuryAllocation 0 --outputPath integration-tests/assets/full_snapshot.bin
+rm integration-tests/assets/full_snapshot.bin && go run main.go tools snap-gen --networkName alphanet1 --mintAddress 6920b176f613ec7be59e68fc68f597eb3393af80f74c7c3db78198147d5f1f92 --treasuryAllocation 0 --outputPath integration-tests/assets/full_snapshot.bin
 
 # return to this directory
 cd integration-tests/
