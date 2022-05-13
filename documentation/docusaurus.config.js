992693addeaf889441f26a04389f520bd1ddd575
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-13 09:49:32 +0200

    Prepare for new wiki build system (#1371)
    * Fix package.json
    
    * Update to yarn 3.2.0 and add wiki cli
    
    * Prepare for new wiki build system
    
    * Cleanup PR and update cli
    
    * Add test action
    
    * Fix dependency name
    
    * Update docusaurus.config.js
    
    * Update dependencies
    
    * Add cli build

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'hornet',
+                path: path.resolve(__dirname, 'docs'),
+                routeBasePath: 'hornet',
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+                editUrl: 'https://github.com/iotaledger/hornet/edit/mainnet/documentation',
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, 'static')],
+};
