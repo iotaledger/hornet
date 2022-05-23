0aadf8ccb69654c3eceb80cf35c0433727879a5d
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-24 01:01:20 +0200

    Add new build system for docs

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'hornet-develop',
+                path: path.resolve(__dirname, 'docs'),
+                routeBasePath: 'hornet',
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+                editUrl: 'https://github.com/iotaledger/hornet/edit/develop/documentation',
+                versions: {
+                    current: {
+                        label: 'Develop',
+                        path: 'develop',
+                        badge: true
+                    },
+                },
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, 'static')],
+};
