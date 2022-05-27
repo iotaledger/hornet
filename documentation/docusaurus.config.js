4a515436f10c5778c2bd4182774f69526df22e12
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-27 11:45:32 +0200

    Fix GitHub org

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'hornet',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
-                editUrl: 'https://github.com/iotaledger/hornet/edit/develop/documentation',
+                editUrl: 'https://github.com/gohornet/hornet/edit/develop/documentation',
                 versions: {
                     current: {
                         label: 'Develop',
