d1a7ebc7014913c0fdca29a74d09a3d54464412a
Alexander Sporn <github@alexsporn.de>
2022-07-15 15:55:16 +0200

    - Update all inx docker versions (#1625)

--- integration-tests/runTests.sh
+++ integration-tests/runTests.sh
@@ -16,8 +16,8 @@ then
   fi
 fi
 
-docker pull iotaledger/inx-coordinator:0.8
-docker pull iotaledger/inx-indexer:0.10
+docker pull iotaledger/inx-coordinator:1.0.0-beta.1
+docker pull iotaledger/inx-indexer:1.0.0-beta.1
 
 echo "Run integration tests"
 for name in $TEST_NAMES; do
--- integration-tests/tester/framework/config.go
+++ integration-tests/tester/framework/config.go
@@ -28,8 +28,8 @@ const (
 	autopeeringMaxTries = 50
 
 	containerNodeImage           = "hornet:dev"
-	coordinatorImage             = "iotaledger/inx-coordinator:0.8"
-	indexerImage                 = "iotaledger/inx-indexer:0.10"
+	coordinatorImage             = "iotaledger/inx-coordinator:1.0.0-beta.1"
+	indexerImage                 = "iotaledger/inx-indexer:1.0.0-beta.1"
 	containerWhiteFlagMockServer = "wfmock:latest"
 
 	containerNameTester  = "/tester"
