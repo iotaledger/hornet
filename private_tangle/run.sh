d514a18ee6af25f04206ebe53c0f19e3bf1aabe5
Eike Haß <eike-hass@web.de>
2022-07-20 11:16:46 +0200

    pipe extra arguments to up command

--- private_tangle/run.sh
+++ private_tangle/run.sh
@@ -5,10 +5,16 @@ if [ ! -d "privatedb" ]; then
   exit
 fi
 
-if [ $# -eq 0 ]; then
-    docker compose --profile "2-nodes" up
-elif [[ $1 = "3" || $1 = "4" ]]; then
-    docker compose --profile "$1-nodes" up
+# first arguments must be '3|4' and extra argument must not exist or must start with '-'
+if [[ ($1 = "3" || $1 = "4") && ($2 = "" || $2 = -*) ]]; then
+    PROFILE=$1
+    # shift arguments to remove profile
+    shift;
+    docker compose --profile "$PROFILE-nodes" up $@
+
+# argument must not exist or must start with '-'
+elif [[ $1 = "" || $1 = -* ]]; then
+    docker compose --profile "2-nodes" up $@ 
 else
-  echo "Usage: ./run.sh [3|4]"
+  echo "Usage: ./run.sh [3|4] [docker-compose up options]"
 fi
