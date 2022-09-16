14b49401c04e6bcb469d63fa7266e8daec375e0a
Alexander Sporn <github@alexsporn.de>
2022-09-16 14:13:02 +0200

    Use new 1.0-beta tags for inx containers

--- integration-tests/runTests.sh
+++ integration-tests/runTests.sh
@@ -16,8 +16,8 @@ then
   fi
 fi
 
-docker pull iotaledger/inx-coordinator:1.0.0-beta.7
-docker pull iotaledger/inx-indexer:1.0.0-beta.7
+docker pull iotaledger/inx-coordinator:1.0-beta
+docker pull iotaledger/inx-indexer:1.0-beta
 
 echo "Run integration tests"
 for name in $TEST_NAMES; do
