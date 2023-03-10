9536ea47dbee0033a5b9da74329255cdf7703047
muXxer <git@muxxer.de>
2023-03-10 12:25:50 +0100

    Use pushd and popd

--- scripts/gendoc.sh
+++ scripts/gendoc.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-CURRENT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-PARENT_DIR="$( builtin cd ${CURRENT_DIR}/.. >/dev/null 2>&1 ; pwd -P )"
-cd ${PARENT_DIR}
-
-cd tools/gendoc
+pushd ../tools/gendoc
 
 # determine current HORNET version tag
 commit_hash=$(git rev-parse --short HEAD)
@@ -13,5 +9,4 @@ BUILD_LD_FLAGS="-X=github.com/iotaledger/hornet/v2/core/app.Version=${commit_has
 
 go run -tags ${BUILD_TAGS} -ldflags ${BUILD_LD_FLAGS} main.go
 
-cd ${CURRENT_DIR}
-
+popd
--- scripts/go_mod_tidy.sh
+++ scripts/go_mod_tidy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-CURRENT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-PARENT_DIR="$( builtin cd ${CURRENT_DIR}/.. >/dev/null 2>&1 ; pwd -P )"
-cd ${PARENT_DIR}
+pushd ./..
 
 go mod tidy
 
-cd ${PARENT_DIR}/tools/gendoc
+cd tools/gendoc
+
 go mod tidy
 
-cd ${CURRENT_DIR}
+popd
