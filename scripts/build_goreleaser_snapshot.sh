f064a26d9e8d001ca0561df266a4799df86a8d4c
muXxer <git@muxxer.de>
2023-08-09 18:40:13 +0200

    Update workflows and goreleaser

--- scripts/build_goreleaser_snapshot.sh
+++ scripts/build_goreleaser_snapshot.sh
@@ -10,4 +10,4 @@ GORELEASER_IMAGE=iotaledger/goreleaser-cgo-cross-compiler:1.20.2
 REPO_PATH="/build"
 
 docker pull "${GORELEASER_IMAGE}"
-docker run --rm --privileged -v "${PWD}":"${REPO_PATH}" -w "${REPO_PATH}" "${GORELEASER_IMAGE}" goreleaser --rm-dist --snapshot --skip-publish
+docker run --rm --privileged -v "${PWD}":"${REPO_PATH}" -w "${REPO_PATH}" "${GORELEASER_IMAGE}" goreleaser --snapshot --skip-publish --clean
