3ab3a6d82412251a0db62657bd44ad537280db4d
muXxer <git@muxxer.de>
2023-08-09 11:19:14 +0200

    Update workflows and goreleaser

--- scripts/build_goreleaser_snapshot.sh
+++ scripts/build_goreleaser_snapshot.sh
@@ -6,8 +6,8 @@
 # make script executable independent of path
 cd $(dirname "$0")/../
 
-GORELEASER_IMAGE=iotmod/goreleaser-cgo-cross-compiler:1.15.5
+GORELEASER_IMAGE=iotaledger/goreleaser-cgo-cross-compiler:1.20.2
 REPO_PATH="/build"
 
 docker pull "${GORELEASER_IMAGE}"
-docker run --rm --privileged -v "${PWD}":"${REPO_PATH}" -w "${REPO_PATH}" "${GORELEASER_IMAGE}" goreleaser --rm-dist --snapshot --skip-publish
+docker run --rm --privileged -v "${PWD}":"${REPO_PATH}" -w "${REPO_PATH}" "${GORELEASER_IMAGE}" goreleaser --snapshot --skip-publish --clean
